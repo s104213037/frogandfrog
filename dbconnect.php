84c1ecf0ab9086372def19ba982173b2a13c4b09
Yorkchung <31153913+Yorkchung@users.noreply.github.com>
2018-01-06 14:03:02 +0800

    Delete dbconnect.php

--- dbconnect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host = 'localhost';
-$user = 'rootfrog';
-$pass = '123456';
-$db = 'frogpicture';
-$conn = mysqli_connect($host, $user, $pass, $db) or die('Error with MySQL connection'); //跟MyMSQL連線並登入
-mysqli_query($conn,"SET NAMES utf8"); //選擇編碼
-?>
