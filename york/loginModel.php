7b2f0e3ad4c768c924ae317bf5be9aae969dab3f
張佑崧 <31153913+Yorkchung@users.noreply.github.com>
2018-01-06 14:21:26 +0800

    Update loginModel.php

--- york/loginModel.php
+++ york/loginModel.php
@@ -4,7 +4,7 @@ require("dbconnect.php"); //匯入連結資料庫之共用程式碼
 function checkUP($userName,$passWord) {
 	global $conn;
 	$userName = mysqli_real_escape_string($conn,$userName); //將特殊SQL字元編碼，以免被SQL Injection
-	$sql = "SELECT password, id FROM user WHERE loginID='$userName'"; //產生SQL指令
+	$sql = "SELECT password, id FROM user WHERE username='$userName'"; //產生SQL指令
 	if ($result = mysqli_query($conn,$sql)) { //執行SQL查詢
 		if ($row=mysqli_fetch_assoc($result)) { //取得第一筆資料
 			if ($row['password'] == $passWord) { //比對密碼
